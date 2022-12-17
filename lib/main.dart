d02be1f608c3a26ba63a0e3fdba726a85a538d84
Fidel AHM <fidelahm10@gmail.com>
2022-12-17 00:22:06 -0800

    new code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expense_app/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +22,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 'tl', title: 'new shoes', amount: 1000, date: DateTime.now()),
+    Transaction(id: 't2', title: 'note book', amount: 100, date: DateTime.now())
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,19 +35,50 @@ class MyHomePage extends StatelessWidget {
         title: Text('Expense App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
         children: [
           Container(
             width: double.infinity,
             child: Card(
               color: Colors.indigoAccent,
-              child: Container(child: Text('Chart')),
+              child: Text('Chart'),
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('List of TX'),
-          )
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+                child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.indigoAccent, width: 2)),
+                  child: Text(
+                    '\$${tx.amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.indigoAccent),
+                  ),
+                  padding: EdgeInsets.all(10),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tx.title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    Text(
+                      DateFormat.yMMMEd().format(tx.date),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                )
+              ],
+            ));
+          }).toList())
         ],
       ),
     );
